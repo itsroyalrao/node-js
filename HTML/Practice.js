f58ce5b9b7b1e16fd9ed5aef6ca1525d1bf5b9c5
itsroyalrao <my701319>
2023-07-16 17:38:22 +0530

    practice.html

---
+++ HTML/Practice.js
@@ -0,0 +1,5 @@
+const liItem = document.getElementsByClassName('item');
+    liItem[0].textContent = 'HELLO';
+    liItem[0].style.backgroundColor = 'green';
+    liItem[1].style.backgroundColor = 'yellow';
+    console.log(liItem[0].textContent);
