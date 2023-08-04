b9b94432700ccc9a51e756957709c8b4eeda3dc0
itsroyalrao <my701319@gmail.com>
2023-08-04 18:51:20 +0530

    writing and reading from a file

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
 
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
-
     const url = req.url;
+    const method = req.method;
 
-    if (url === '/home') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Home</title></head>');
-        res.write('<body><h4>Welcome home</h4></body>');
-        res.write('</html>');
-        return res.end();
-    } else if (url === '/about') {
-        res.setHeader('Content-Type', 'text/html');
+    if (url === '/') {
+        // const parsedBody = Buffer.concat(body).toString();
+        // const message = parsedBody.split('=')[1];
+        // console.log(message);
+
         res.write('<html>');
         res.write('<head><title>About Us</title></head>');
-        res.write('<body><h4>Welcome to About Us page</h4></body>');
-        res.write('</html>');
-        return res.end();
-    } else if (url === '/node') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Node JS</title></head>');
-        res.write('<body><h4>Welcome to my Node Js project</h4></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>');
         res.write('</html>');
         return res.end();
     }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', chunk => {
+            console.log(chunk);
+            body.push(chunk);
+            console.log(`chunk is ${chunk}`);
+        })
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            // console.log(message);
+
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+
+                return res.end();
+            });
+        })
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Home</title></head>');
+    res.write('<body><h4>hello world</h4></body>');
+    res.write('</html>');
+    res.end();
+
 })
 
 server.listen(4000)
