f603fe28324e275674d9a00f89412d4fb7b6ae17
my701 <my701319@gmail.com>
2023-08-23 12:34:35 +0530

    Cart with many to many relationship

--- app.js
+++ app.js
@@ -1,16 +1,72 @@
-const express = require("express");
-const expenseRoutes = require("./routes/expenseRoutes");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
+
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use("/", expenseRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("*", (req, res) => {
-  res.status(404).json({ msg: "Page Not Found!" });
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
-app.listen(3000);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findById(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(4000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- routes/expenseRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const expenseController = require("../controllers/expenseController");
-const router = express.Router();
-
-router.get("/", expenseController.getAllExpenses);
-router.post("/", expenseController.createNewExpense);
-router.post("/:id", expenseController.deleteExpense);
-
-module.exports = router;
