9381ea21c0aab3a2552eab09e646a73a4b9c83b3
my701 <my701319@gmail.com>
2023-08-27 18:13:05 +0530

    Make the login backend api

--- controllers/login.js
+++ controllers/login.js
@@ -1,14 +1,14 @@
-const bcrypt = require('bcrypt');
 const Signup = require('../models/signup');
 
 const login = async (req, res) => {
   try {
     const user = await Signup.findOne({ email: req.body.email });
-    if (!user) return res.status(200).json({ success: false, msg: "Email is incorrect!" });
-    const passwordMatch = await bcrypt.compare(req.body.password, user.password);
-    console.log(user.password);
-    if (passwordMatch) return res.status(200).json({ success: true, msg: 'Logged in successfully!' });
-    else return res.status(200).json({ success: true, msg: 'Password do not match!' });
+    if (!user) return res.status(404).json({ success: false, msg: "Email is incorrect!" });
+
+    const passwordMatch = user.password === req.body.password;
+    if (passwordMatch) return res.status(200).json({ success: true, msg: 'User login sucessful!' });
+    else return res.status(401).json({ success: false, msg: 'Password do not match!' });
+
   } catch (error) {
     console.log(error);
   }
--- controllers/signup.js
+++ controllers/signup.js
@@ -3,7 +3,7 @@ const Signup = require('../models/signup');
 const signup = async (req, res) => {
   try {
     const user = await Signup.findOne({ email: req.body.email });
-    if (user) return res.status(200).json({ success: false, msg: 'User already exists!' });
+    if (user) return res.status(500).json({ success: false, msg: 'User already exists!' });
     await Signup.create(req.body);
     res.status(200).json({ success: true, msg: 'User is added successfully!' })
   } catch (error) {
