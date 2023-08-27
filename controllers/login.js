f4cccd8f9094732a5a33e59ef825159035a7d557
my701 <my701319@gmail.com>
2023-08-27 19:14:03 +0530

    Your user's password is at risk!!!!

--- controllers/login.js
+++ controllers/login.js
@@ -1,14 +1,16 @@
+const bcrypt = require('bcrypt');
 const Signup = require('../models/signup');
 
 const login = async (req, res) => {
   try {
-    const user = await Signup.findOne({ email: req.body.email });
+    const { email, password } = req.body;
+    const user = await Signup.findOne({ email: email });
     if (!user) return res.status(404).json({ success: false, msg: "Email is incorrect!" });
 
-    const passwordMatch = user.password === req.body.password;
-    if (passwordMatch) return res.status(200).json({ success: true, msg: 'User login sucessful!' });
-    else return res.status(401).json({ success: false, msg: 'Password do not match!' });
-
+    const passwordMatch = bcrypt.compare(password, user.password, (err, same) => {
+      if (same) return res.status(200).json({ success: true, msg: 'User login sucessful!' });
+      else return res.status(401).json({ success: false, msg: 'Password do not match!' });
+    });
   } catch (error) {
     console.log(error);
   }
--- controllers/signup.js
+++ controllers/signup.js
@@ -1,11 +1,17 @@
+const bcrypt = require('bcrypt');
 const Signup = require('../models/signup');
 
 const signup = async (req, res) => {
   try {
-    const user = await Signup.findOne({ email: req.body.email });
+    const { name, email, password } = req.body;
+    const user = await Signup.findOne({ email: email });
     if (user) return res.status(500).json({ success: false, msg: 'User already exists!' });
-    await Signup.create(req.body);
-    res.status(200).json({ success: true, msg: 'User is added successfully!' })
+    bcrypt.hash(password, 10, async (err, encrypted) => {
+      console.log(err);
+
+      await Signup.create({ name, email, password: encrypted });
+      res.status(200).json({ success: true, msg: 'User is added successfully!' })
+    })
   } catch (error) {
     console.log(error);
   }
